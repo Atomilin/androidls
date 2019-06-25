90038a7aed7c653eaf261f8c2c6a39570bef9b65
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-06-25 10:31:30 +0300

    add item to top of list

--- app/src/main/java/com/example/loftmoney/ItemsAdapter.java
+++ app/src/main/java/com/example/loftmoney/ItemsAdapter.java
@@ -33,8 +33,8 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHold
     }
 
     public void addItem (final Item item){
-        mItemList.add(item);
-        notifyItemInserted(mItemList.size());
+        mItemList.add(0, item);
+        notifyItemInserted(0);
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
