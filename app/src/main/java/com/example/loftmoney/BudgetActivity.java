e8b1c89ac713b93299a5f4f29540903f2a533d7a
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-06-25 09:36:32 +0300

    Added empty field check

--- app/src/main/java/com/example/loftmoney/BudgetActivity.java
+++ app/src/main/java/com/example/loftmoney/BudgetActivity.java
@@ -1,8 +1,10 @@
 package com.example.loftmoney;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -11,11 +13,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class BudgetActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 1001;
     private ItemsAdapter mItemsAdapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
@@ -30,7 +35,7 @@ public class BudgetActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        dividerItemDecoration.setDrawable(ContextCompat.getDrawable(this, R.drawable.divaider));
+        dividerItemDecoration.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(this, R.drawable.divaider)));
         recyclerView.addItemDecoration(dividerItemDecoration);
 
         mItemsAdapter.addItem(new Item("Молоко", 70));
@@ -51,7 +56,10 @@ public class BudgetActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            Item item = new Item(data.getStringExtra("name"), Integer.parseInt(data.getStringExtra("price")));
+            Item item = null;
+            if (data != null) {
+                item = new Item(data.getStringExtra("name"), Integer.parseInt(data.getStringExtra("price")));
+            }
             mItemsAdapter.addItem(item);
         }
     }
